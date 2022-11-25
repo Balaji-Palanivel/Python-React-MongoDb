293167f9100cc310f843349cfdc2a50f291caff3
BALAJI PALANIVEL <107667070+Balaji-Palanivel@users.noreply.github.com>
2022-11-25 17:56:33 +0530

    Added POST option

--- flask/base.py
+++ flask/base.py
@@ -1,4 +1,3 @@
-
 from flask import Flask,render_template
 from flask_pymongo import PyMongo
 from flask import jsonify,request
@@ -10,7 +9,7 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://127.0.0.1:27017/database_1"
 mongo = PyMongo(app)
 all_users = mongo.db.collection_3
-
+filter_status = 'false'
 
 @app.route("/",methods=['GET'])
 def get_details():    
@@ -26,26 +25,33 @@ def get_details():
         
     
 def home_page():
+    global filter_status
     x = all_users.find()
     y = dumps(x) 
     authors = json.loads(y)
+    filter_status = 'false'
     return authors    
 
 def sort_Asc():
-    x = all_users.find().sort("name",1)
-    y = dumps(x) 
-    authors = json.loads(y)
-    print("Sort Asc called..........")
-    return authors
+    if filter_status == 'true':
+        return (sorted(filterd_1, key=lambda i: (i['name']),reverse=False))
+    elif filter_status == 'false':
+        x = all_users.find().sort("name",1)
+        y = dumps(x) 
+        authors = json.loads(y)
+        return authors
 
 def sort_Dsc():
-    x = all_users.find().sort("name",-1)
-    y = dumps(x) 
-    authors = json.loads(y)
-    print("Sort Dsc called..........")
-    return authors
+    if filter_status == 'true':
+        return (sorted(filterd_1, key=lambda i: (i['name']),reverse=True))
+    elif filter_status == 'false':
+        x = all_users.find().sort("name",-1)
+        y = dumps(x) 
+        authors = json.loads(y)
+        return authors
 
 def filter(name):
+    global filterd_1,filter_status
     x = all_users.find()
     y = dumps(list(x), indent = 2) 
     authors = json.loads(y)
@@ -55,22 +61,22 @@ def filter(name):
             z.append(x)
     a = dumps(z, indent = 2) 
     b = json.loads(a)
-    print("Filter fun  called..........")
+    filterd_1,filter_status = b, 'true'
     if request.args.get('sort') == 'asc':
       return (sorted(b, key=lambda i: (i['name']),reverse=False))
     elif request.args.get('sort') == 'dsc':
        return (sorted(b, key=lambda i: (i['name']),reverse=True))
     
 
-@app.route("/add",methods=['POST'])
+@app.route("/add",methods=['GET','POST'])
 def find_id():
     req = request.get_json()   
     NAME = req['name']
     EMAIL = req['email']
-    PWD = req['pwd']   
+    # PWD = req['pwd']   
     all_users.create_index("name", unique=True)
     try :
-        all_users.insert_one({"name" : NAME, "email" : EMAIL, "pwd" : PWD})
+        all_users.insert_one({"name" : NAME, "email" : EMAIL})
     except :
         return "Given NAME is already exist......" 
     return "Successfully Added...."
--- src/App.js
+++ src/App.js
@@ -3,93 +3,76 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import axios from "axios";
+import $ from 'jquery';
 
 export default class App extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        data1: [],
-        URL: "http://127.0.0.1:5000/",
-        sort_item: "asc"
-        
-      };
-      this.api_call(this.state.URL)
-      this.sorting_item = this.sorting_item.bind(this)
-    }
-    api_call(url1) {
-      axios({
-        method: "GET",
-        url: url1,
+  constructor(props) {
+    super(props);
+    this.state = {
+      data1: [],
+      URL: "http://127.0.0.1:5000/",
+      sort_item: "asc",
+      filterd: false,
+      add_page: true
+
+    };
+    this.api_call(this.state.URL)
+    this.sorting_item = this.sorting_item.bind(this)
+  }
+  api_call(url1) {
+    console.log("helooooooo")
+    axios({
+      method: "GET",
+      url: url1,
+    })
+      .then((response) => {
+        this.setState({ 'data1': response.data })
       })
-        .then((response) => {
-          this.setState({ 'data1': response.data })
-        })
+  }
+  sorting_item(e) {
+    if (this.state.sort_item == 'asc') {
+      this.api_call("http://127.0.0.1:5000/?sort=asc")
+      this.setState({ sort_item: 'dsc' })
     }
-    sorting_item(e) {      
-      if (this.state.sort_item == 'asc') {
-        this.api_call("http://127.0.0.1:5000/?sort=asc") 
-        this.setState({ sort_item : 'dsc'})   
-      }
-      if (this.state.sort_item == 'dsc') {
-        this.api_call("http://127.0.0.1:5000/?sort=dsc")
-        this.setState({ sort_item : 'asc'})  
+    if (this.state.sort_item == 'dsc') {
+      this.api_call("http://127.0.0.1:5000/?sort=dsc")
+      this.setState({ sort_item: 'asc' })
+    }
+
+  }
+  filtered_item(item) {
+    if (item.length > 0) {
+     if (this.state.sort_item == 'asc') {
+      this.api_call("http://127.0.0.1:5000/?sort=asc" + '&filter=' + item)
+    }
+     if (this.state.sort_item == 'dsc') {
+      this.api_call("http://127.0.0.1:5000/?sort=dsc" + '&filter=' + item)
     }
   }
-    filtered_item(item){  
-      if (this.state.sort_item == 'asc') {        
-        this.api_call("http://127.0.0.1:5000/?sort=asc"+'&filter='+item)
-      }
-      if (this.state.sort_item == 'dsc') {
-        this.api_call("http://127.0.0.1:5000/?sort=dsc"+'&filter='+item)
-     }
-    }
-
-
-
-//   api_call() {
-//     console.log("Again again")
-//     axios({
-//       method: "GET",
-//       url: this.state.URL,
-//     })
-//       .then((response) => {
-//         this.setState({ 'data1': response.data })
-//       })
-//   }
-//   sorting_item(e) {
+}
+  add_new_ele(e) {
     
-//     if (this.state.sort_item == 'asc') {
-//       console.log("sort asc")
-//       this.setState({ URL: "http://127.0.0.1:5000/?sort=asc", sort_item: 'dsc' });
-//       this.api_call()
-//     }
-//     if (this.state.sort_item == 'dsc') {
-//       console.log("sort dsc")
-//       this.setState({ URL: "http://127.0.0.1:5000/?sort=dsc", sort_item: 'asc' });
-//       this.api_call()
-//     }
-//   }
-//   filtered_item(item){  
-//     if (this.state.sort_item == 'asc') {
-//       this.setState({ URL: "http://127.0.0.1:5000/?sort=asc"+'&filter='+item });
-//       this.api_call()
-//     }
-//     if (this.state.sort_item == 'dsc') {
-//       this.setState({ URL: "http://127.0.0.1:5000/?sort=dsc"+'&filter='+item });
-//       this.api_call()
-//     }
-//    }
+    $.ajax({
+      type: "POST",
+      url: "http://127.0.0.1:5000/add",
+      contentType: "application/json",
+      data: JSON.stringify({ name: document.getElementById('form4Example2').value, email: document.getElementById('form4Example4').value }),
+      dataType: "json"
+    });
+    window.location.reload();
+  }
+
 
   render() {
     return (
-      <div>        
-          <div className='row'>
-          <div className="input-group" style={{Left: "10px"}}>
+      <div>
+        <div className='row'>
+          <div className="input-group" style={{ Left: "10px" }}>
             <div id="search-autocomplete" className="col-sm-2 offset-sm-9 mt-5 mb-4 form-outline">
               <input type="search" id="form1" className="form-control" />
             </div>
-            <button type="button" className="btn btn-primary" style={{marginTop : "48px",marginBottom : "57px"}}>
-              <i onClick={e => this.filtered_item(document.getElementById('form1').value)} className="fa fa-fw fa-search"></i>
+            <button onClick={e => this.filtered_item(document.getElementById('form1').value.toLowerCase())} type="button" className="btn btn-primary" style={{ marginTop: "48px", marginBottom: "57px" }}>
+              <i className="fa fa-fw fa-search"></i>
             </button>
           </div>
 
@@ -113,6 +96,46 @@ export default class App extends Component {
             </tbody>
           </table>
         </div>
+
+
+
+        <button type="button" class="btn btn-lg btn-primary" style={{ marginLeft: "900px" }} data-mdb-toggle="modal" data-mdb-target="#exampleModal">
+          ADD
+        </button>
+
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Add New Profile</h5>
+                <button type="button" class="btn-close" data-mdb-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                <div className="card-body" >
+                  <div className="container d-flex ">
+
+                    <form >
+                      <div class="input-group flex-nowrap">
+                        <span class="input-group-text" id="addon-wrapping">Name </span>
+                        <input type="text" class="form-control" id="form4Example2" aria-label="Username" aria-describedby="addon-wrapping" />
+                      </div><br />
+                      <div class="input-group flex-nowrap">
+                        <span class="input-group-text" id="addon-wrapping">Email </span>
+                        <input type="text" class="form-control" id="form4Example4" aria-label="Username" aria-describedby="addon-wrapping" />
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">Close</button>
+                <button type="button" data-mdb-dismiss="modal" onClick={e => this.add_new_ele(e)} class="btn btn-primary">Save</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
 
     )
